4bd11a3ef4ba21d48390d8463ff476d7245dc03e
Ben Ritchie <brit8199@uni.sydney.edu.au>
2025-10-31 09:38:54 +1100

    Changed satellite model and fixed up rotation_mat.py

--- src/rotation_mat.py
+++ src/rotation_mat.py
@@ -46,9 +46,9 @@ def get_Rt(a, b): # Written by chat, unverified
     # Verify mapping
     print("\nCheck mapping:")
     print("Predicted B =")
-    print((R @ A.T).T + t)
+    print((R @ a.T).T + t)
     print("\nDifference:")
-    print(B - ((R @ A.T).T + t))
+    print(b - ((R @ a.T).T + t))
 
     
     return 
@@ -57,21 +57,37 @@ def get_Rt(a, b): # Written by chat, unverified
 print("\n\n====== Manual ====== ") 
 
 # Two vectors to the same point in different coordinate frames 
-a = np.array([ 37.41, 105, 29.1 ])
-b = np.array([ 35.25, 12.43, 27.59 ])
+a = np.array([ -46.08, -12.43, 27.59 ])
+b = np.array([ 105, -12.57, -30.9 ])
 
-# Rotation around z axis 
-rmat_manual = np.array([
-    [0, -1, 0], 
-    [1, 0, 0], 
-    [0, 0, 1]
-]) 
+# Rotation about X axis
+roll = np.deg2rad(180)
+Rx = np.array([ [1, 0, 0], 
+                [0, np.cos(roll), -np.sin(roll)], 
+                [0, np.sin(roll), np.cos(roll)] ])
+
+# Rotation about Y axis
+pitch = np.deg2rad(0)    
+Ry = np.array([ [np.cos(pitch), 0, np.sin(pitch)], 
+                [0, 1, 0], 
+                [-np.sin(pitch), 0, np.cos(pitch)] ])
+
+# Rotation about Z axis
+yaw = np.deg2rad(0)      
+Rz = np.array([ [np.cos(yaw), -np.sin(yaw), 0], 
+                [np.sin(yaw), np.cos(yaw), 0], 
+                [0, 0, 1 ] ]) 
+
+rmat= Rz @ Ry @ Rx 
 # Translation between coordinate frames 
-t_manual = np.array([140.25, -24.98, -1.51])
+t = b - rmat @ a
 
 print("Rotation matrix: ")
-print(rmat_manual) 
+print(rmat) 
 
-print( f"Manual Test: b = {b} = R @ a + t = {(rmat_manual @ a) + t_manual}",  ) 
-print( f"b - (R @ a + t) = { b - ((rmat_manual @ a) + t_manual) }") 
+print("\nTranslation Vector: ")
+print(t)
+
+print( f"\nManual Test: b = {b} = R @ a + t = {(rmat @ a) + t}",  ) 
+print( f"\nb - (R @ a + t) = { b - ((rmat @ a) + t) }") 
 
